912350783344f5cab6722c5152a2476f43dc86e5
Antony.Glim <Antony.Glim@gmail.com>
2020-01-29 11:23:01 +0300

    Initial commit

---
+++ src/main/java/readerblog/mates/ReaderBlog/ReaderBlogApplication.java
@@ -0,0 +1,13 @@
+package readerblog.mates.ReaderBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/readerblog/mates/ReaderBlog/ReaderBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package readerblog.mates.ReaderBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
