57551b475f76e7642e4afb97acd133fecd762762
haroldekb <haroldekb@mail.ru>
2020-02-21 03:10:52 +0500

    Correct few mismatches before pull request

--- src/main/java/readerblog/mates/readerblog/entities/Author.java
+++ src/main/java/readerblog/mates/readerblog/entities/Author.java
@@ -1,15 +1,21 @@
-/*
-  @author haroldEkb@mail.ru
- */
-
 package readerblog.mates.readerblog.entities;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
-import java.time.LocalDate;
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * @author mzheldin@yandex.ru
+ */
 
 @Entity
-@Table(name = "authors")
+@Data
+@NoArgsConstructor
 public class Author {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -28,76 +34,14 @@ public class Author {
     private String shortBiography;
 
     @Column(name = "born_date")
-    private LocalDate bornDate;
+    private Date bornDate;
 
     @Column(name = "rating")
     private Double rating;
 
-    public Author() {
-    }
-
-    public Author(String firstName, String lastName, String patronymicName, String shortBiography, LocalDate bornDate, Double rating) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.patronymicName = patronymicName;
-        this.shortBiography = shortBiography;
-        this.bornDate = bornDate;
-        this.rating = rating;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPatronymicName() {
-        return patronymicName;
-    }
-
-    public void setPatronymicName(String patronymicName) {
-        this.patronymicName = patronymicName;
-    }
-
-    public String getShortBiography() {
-        return shortBiography;
-    }
-
-    public void setShortBiography(String shortBiography) {
-        this.shortBiography = shortBiography;
-    }
-
-    public LocalDate getBornDate() {
-        return bornDate;
-    }
-
-    public void setBornDate(LocalDate bornDate) {
-        this.bornDate = bornDate;
-    }
-
-    public Double getRating() {
-        return rating;
-    }
-
-    public void setRating(Double rating) {
-        this.rating = rating;
-    }
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "authors_books",
+            joinColumns = @JoinColumn(name = "author_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id"))
+    private List<Book> books;
 }
