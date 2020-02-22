f79676581366742c2cdebe741def37cef44c83aa
Antony.Glim <Antony.Glim@gmail.com>
2020-02-22 12:39:24 +0300

    Add annotations and questions to Role and User

--- src/main/java/readerblog/mates/readerblog/entities/Role.java
+++ src/main/java/readerblog/mates/readerblog/entities/Role.java
@@ -1,21 +1,40 @@
 package readerblog.mates.readerblog.entities;
 
-/**
- * @author Sergey Petukhov
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 import java.io.Serializable;
 
+/**
+ * @author Sergey Petukhov
+ */
+
+@Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
 public class Role implements Serializable {
+
+    private static final long serialVersionUID = -7483309127820484301L;
+
+    /* TODO почему поле называется role_id если в базе у нас id
+       TODO зачем нам здесь @OneToOne(optional = false, mappedBy = "role")
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    @OneToOne(optional = false, mappedBy="role")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @OneToOne(optional = false, mappedBy = "role")
     private Long role_id;
+     */
 
-    String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-    public Role(){}
+    @Column(name = "name")
+    private String name;
+
 }
--- src/main/java/readerblog/mates/readerblog/entities/User.java
+++ src/main/java/readerblog/mates/readerblog/entities/User.java
@@ -1,51 +1,70 @@
 package readerblog.mates.readerblog.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import readerblog.mates.readerblog.enums.StatusOfUser;
+
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Sergey Petukhov
  */
 
-import readerblog.mates.readerblog.enums.StatusOfUser;
-import javax.persistence.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+@Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(
         name = "users",
         uniqueConstraints = {@UniqueConstraint(columnNames = {"nick_name", "email"})}
 )
 public class User {
-    public User(){}
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long user_id;
-    private String first_name;
-    private String last_name;
-    private String nick_name;
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "nick_name")
+    private String nickName;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "email")
     private String email;
 
+    /* TODO автор точно уверен в этой конструкции? */
     @OneToOne(optional = false, fetch = FetchType.EAGER)
     @JoinColumn(name = "role", nullable = false)
     private Role role;
 
+    /*TODO поля нет в БД так и должно быть?*/
     private StatusOfUser status;
 
-    // получение информации о пользователе по nick
-    public Map<String, String> getUserInfo(){
+    /**
+     * @return - information about user by nick
+     */
+    public Map<String, String> getUserInfo() {
         Map<String, String> info = new HashMap<>();
-        info.put("first_name",first_name);
-        info.put("last_name",last_name);
-        info.put("nick_name",nick_name);
-        info.put("email",email);
-        info.put("role",StatusOfUser.valueOf("name").toString());
+        info.put("first_name", firstName);
+        info.put("last_name", lastName);
+        info.put("nick_name", nickName);
+        info.put("email", email);
+        info.put("role", StatusOfUser.valueOf("name").toString());
         return info;
     }
 
 // методы изменения статуса, изменения роли, никнейма и пароля
-// - это обычные сеттеры, т.к. у нас подключен Lombok, не объевлены в классе
 
 }
 
