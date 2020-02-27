b4a3b50a0639da5d52904bcb3ac843b25fdf059d
haroldekb <haroldekb@mail.ru>
2020-02-28 01:19:54 +0500

    Try to solve some merge problem

--- src/main/java/readerblog/mates/readerblog/entities/Category.java
+++ src/main/java/readerblog/mates/readerblog/entities/Category.java
@@ -1,28 +1,24 @@
-package readerblog.mates.readerblog.entities;
+package readerblog.mates.ReaderBlog.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
-import java.util.List;
 
-/**
- * @author mzheldin@yandex.ru
- */
-
-@Entity
 @Data
+@Entity
+@AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "categories")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+    private Integer id;
 
-    @Column(name = "name")
     private String name;
 
-    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
-    private List<Book> books;
+    public Category(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/readerblog/mates/readerblog/entities/Genre.java
+++ src/main/java/readerblog/mates/readerblog/entities/Genre.java
@@ -1,28 +1,18 @@
-package readerblog.mates.readerblog.entities;
+package readerblog.mates.ReaderBlog.entities;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
-import java.util.List;
 
-/**
- * @author mzheldin@yandex.ru
- */
-
-@Entity
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name = "genres")
 public class Genre {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+    private Integer id;
 
-    @Column(name = "name")
     private String name;
-
-    @OneToMany(mappedBy = "genre", fetch = FetchType.LAZY)
-    private List<Book> books;
 }
