a04d2211c5bcfef193864773777461ee24f978e0
SergeyViktorovichPetukhov <61189464+SergeyViktorovichPetukhov@users.noreply.github.com>
2020-02-29 11:29:37 +0300

    Add interface UserService
    UserService added

---
+++ src/main/java/readerblog/mates/readerblog/services/UserService.java
@@ -0,0 +1,41 @@
+package readerblog.mates.readerblog.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import readerblog.mates.readerblog.entities.Author;
+import readerblog.mates.readerblog.entities.Book;
+import readerblog.mates.readerblog.entities.Role;
+import readerblog.mates.readerblog.entities.User;
+import readerblog.mates.readerblog.enums.StatusOfUser;
+
+import java.util.List;
+
+/**
+ * @author Sergey Petukhov
+ */
+public interface UserService {
+
+    void deleteByEmail(String email);
+
+    void deleteById(Long id);
+
+    boolean existsByEmail(String email);
+
+    User save (User user);
+
+    User findOneById(Long id);
+
+    List<User> findByRoles(List<Role> roles);
+
+    List<User> findAll();
+
+    User findByFirstNameAndLastName(String firstName, String lastName);
+
+    User findAllByEmail(String email);
+
+    User findByNickName(String nickName);
+
+    List<User> findByStatus(StatusOfUser status);
+
+}
