15d022627e41e7ecbd78717b8693ecae5d7f970c
SergeyViktorovichPetukhov <61189464+SergeyViktorovichPetukhov@users.noreply.github.com>
2020-02-29 11:28:22 +0300

    Add files via upload
    UserRepository changed

--- src/main/java/readerblog/mates/readerblog/repositories/UserRepository.java
+++ src/main/java/readerblog/mates/readerblog/repositories/UserRepository.java
@@ -8,11 +8,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import readerblog.mates.readerblog.entities.Role;
 import readerblog.mates.readerblog.entities.User;
 import readerblog.mates.readerblog.enums.StatusOfUser;
 
+import java.util.List;
 import java.util.Optional;
-
+/**
+ * @author Sergey Petukhov
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     // методы save и deleteById есть в CrudRepository по умолчанию
@@ -21,5 +25,13 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     boolean existsByEmail(String email);
 
-    Optional<User> findByEmail(String email);
+    User findByEmail(String email);
+
+    List<User> findByRoles(List<Role> roles);
+
+    User findByFirstNameAndLastName(String firstName, String lastName);
+
+    User findByNickName(String nickName);
+
+    List<User> findByStatus(StatusOfUser status);
 }
